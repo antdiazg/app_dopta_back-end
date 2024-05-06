6b4040c0d449aacb9c489f9d161c84cc0305f380
NicEspinozaT <nic.espinozat@duocuc.cl>
2024-05-05 22:21:12 -0400

    Eliminando dependencias preparando prueba aws

--- appdopta_rest/settings/local.py
+++ appdopta_rest/settings/local.py
@@ -8,10 +8,21 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "appdoptadb",
+        "USER": "admin",
+        "PASSWORD": "AY8whUrSkGRvMA1BjUPz",
+        "HOST": "appdoptadb.cbq6e40k09wf.us-east-1.rds.amazonaws.com",
+        "PORT": "3306",
     }
 }
 
